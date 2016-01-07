b6a41b2fc4bc2d749aac88e12c7d5e1748bdb674
nashy3k <nashy3k@gmail.com>
2016-01-07 21:57:07 +0800

    Fixed email regexp

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,10 +41,10 @@ class UserTest < ActiveSupport::TestCase
 		end
 	end	
 
-	test "email validation should areject invalid addresses" do
+	test "email validation should reject invalid addresses" do
 		invalid_addresses = %w[user@exampl,com user_at_foo.org 
 													user.name@example. foo@bar_baz.com 
-													foo@bar+baz.com]
+													foo@bar+baz.com foo@bar..com]
 		invalid_addresses.each do |invalid_address|
 			@user.email = invalid_address
 			assert_not @user.valid?, "Address #{invalid_address.inspect} should be invalid"	
@@ -57,6 +57,13 @@ class UserTest < ActiveSupport::TestCase
 		@user.save
 		assert_not duplicate_user.valid?
 	end
+
+	test "email addresses should be saved as lower-case" do
+	    mixed_case_email = "Foo@ExAMPle.CoM"
+	    @user.email = mixed_case_email
+	    @user.save
+	    assert_equal mixed_case_email.downcase, @user.reload.email
+	  end
 
 	test "password should have a minimum length" do
 		@user.password = @user.password_confirmation ="a" * 5
