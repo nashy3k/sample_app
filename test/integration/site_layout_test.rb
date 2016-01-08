f2f35fb803d524aa8445bb22fce15456919da59b
nashy3k <nashy3k@gmail.com>
2016-01-09 02:27:23 +0800

    Fixed updating-users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,6 +9,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", help_path
 		assert_select "a[href=?]", about_path
 		assert_select "a[href=?]", contact_path
+		assert_select "a[href=?]", login_path
+    user = users(:michael)
+    log_in_as(user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
+    delete logout_path
+		follow_redirect!
+		assert_select "a[href=?]", login_path
+		assert_select "a[href=?]", logout_path, 		 count: 0
+		assert_select "a[href=?]", user_path(user), count: 0
 		get signup_path
 		assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
 		assert_select "title", full_title("Sign up")
