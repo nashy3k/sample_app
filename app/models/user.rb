0c38de0b9fd9dcb6746033c1dc02809456072a83
nashy3k <nashy3k@gmail.com>
2016-01-09 20:01:59 +0800

    Add password & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }
@@ -50,9 +50,26 @@ class User < ActiveRecord::Base
 		update_attribute(:activated_at, Time.zone.now)
 	end
 
-	# Sends activation email
+	# Sends activation email.
 	def send_activation_email
     UserMailer.account_activation(self).deliver_now
+	end
+
+	# Sets the password reset attributes.
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest,  User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)	
+	end
+
+	# Sends password reset email.
+	def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+	end
+
+	# Returns true if a password reset has expired.
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 
 	private
@@ -62,7 +79,7 @@ class User < ActiveRecord::Base
 			self.email = email.downcase
 		end
 
-		# Creates and assigns the ativation token and digest	
+		# Creates and assigns the ativation token and digest.	
 		def create_activation_digest
 			self.activation_token = User.new_token
 			self.activation_digest = User.digest(activation_token)
