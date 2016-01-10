2c07b8358915d63d0a74a5ba5b0de1ff942d8600
nashy3k <nashy3k@gmail.com>
2016-01-10 15:19:48 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :microposts, dependent: :destroy
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest
@@ -75,6 +76,13 @@ class User < ActiveRecord::Base
 		reset_sent_at < 2.hours.ago
 	end
 
+	# Defines a proto-feed
+	# See "Following users" for the full implementation
+	def feed
+		Micropost.where("user_id = ?", id)
+		# self.microposts
+	end
+		
 	private
 
 		# Converts email to all lower-case.
